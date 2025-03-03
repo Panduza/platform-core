b047fec87fc62e0cc5a46576f4dc15f738799233
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2025-03-03 11:44:00 +0100

    update

--- src/model.rs
+++ src/model.rs
@@ -13,5 +13,17 @@ pub trait BooleanAccessorModel: Sync + Send {
     async fn set_boolean_at(&mut self, index: usize, value: bool) -> Result<(), Error>;
 }
 
+#[async_trait]
+///
+///
+pub trait VectorF32AccessorModel: Sync + Send {
+    ///
+    ///
+    async fn get_vectorf32_at(&mut self, index: usize) -> Result<Vec<f32>, Error>;
+    ///
+    ///
+    async fn set_vectorf32_at(&mut self, index: usize, value: Vec<f32>) -> Result<(), Error>;
+}
+
 // acquisitor -> data ro + trigger
 // one or multiple vector of f32
