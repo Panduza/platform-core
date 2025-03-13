ae84b64681278a2f19b3d9f9e27328db4d398c12
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2025-03-13 20:45:23 +0100

    up

--- src/template/class/boolean_acquisitor.rs
+++ src/template/class/boolean_acquisitor.rs
@@ -1,6 +1,6 @@
 use crate::{
-    log_debug_mount_end, log_debug_mount_start, model::BooleanAccessorModel,
-    template::class::trigger, Container, Error,
+    instance::server::boolean::BooleanAttributeServer, log_debug_mount_end, log_debug_mount_start,
+    model::BooleanAccessorModel, template::class::trigger, Container, Error,
 };
 use async_trait::async_trait;
 use std::sync::Arc;
@@ -12,13 +12,14 @@ use super::trigger::Triggerable;
 struct TriggerWrap<I: BooleanAccessorModel> {
     interface: I,
     index: usize,
+    att_srv: BooleanAttributeServer,
 }
 
 #[async_trait]
 impl<I: BooleanAccessorModel> Triggerable for TriggerWrap<I> {
     async fn on_trigger(&mut self) -> Result<(), Error> {
         let value = self.interface.get_boolean_at(self.index).await?;
-        // self.att.set_from_f32(value as f32).await?;
+        self.att_srv.set(value).await?;
         Ok(())
     }
 }
@@ -51,13 +52,14 @@ pub async fn mount<A: Into<String>, C: Container, I: BooleanAccessorModel + Clon
 
     //
     //
-    let trigger_interface = TriggerWrap {
+    let trigger_wrap = TriggerWrap {
         // channel: 0,
         // att: att_data.clone(),
         interface: interface.clone(),
         index: index,
+        att_srv: att_data,
     };
-    trigger::mount(top_class, trigger_interface).await?;
+    trigger::mount(top_class, trigger_wrap).await?;
 
     //
     log_debug_mount_end!(logger);
--- src/template/class/vectorf32_acquisitor.rs
+++ src/template/class/vectorf32_acquisitor.rs
@@ -1,10 +1,29 @@
 use crate::{
-    log_debug_mount_end, log_debug_mount_start, model::VectorF32AccessorModel,
-    template::class::trigger, Container, Error,
+    instance::server::vector_f32_v0::VectorF32AttributeServer, log_debug_mount_end,
+    log_debug_mount_start, model::VectorF32AccessorModel, template::class::trigger, Container,
+    Error,
 };
 use async_trait::async_trait;
 use std::sync::Arc;
 use tokio::sync::Mutex;
+
+use super::trigger::Triggerable;
+
+#[derive(Clone)]
+struct TriggerWrap<I: VectorF32AccessorModel> {
+    interface: I,
+    index: usize,
+    att_srv: VectorF32AttributeServer,
+}
+
+#[async_trait]
+impl<I: VectorF32AccessorModel> Triggerable for TriggerWrap<I> {
+    async fn on_trigger(&mut self) -> Result<(), Error> {
+        let value = self.interface.get_vectorf32_at(self.index).await?;
+        self.att_srv.set(&value).await?;
+        Ok(())
+    }
+}
 
 ///
 ///
@@ -33,20 +52,18 @@ pub async fn mount<A: Into<String>, C: Container, I: VectorF32AccessorModel + Cl
         .await?;
 
     let data = interface.get_vectorf32_at(index).await?;
-    println!("{:?}", data); // //
-                            // //
-                            // let tttt = TriggerableSi {
-                            //     channel: 0,
-                            //     att: att_data.clone(),
-                            //     interface: interface.clone(),
-                            // };
+    println!("{:?}", data);
 
-    // trigger::mount(top_class, Arc::new(Mutex::new(tttt))).await?;
-
-    // data
-    // class trigger
-    //    single
-    //    cyclic
+    //
+    //
+    let trigger_wrap = TriggerWrap {
+        // channel: 0,
+        // att: att_data.clone(),
+        interface: interface.clone(),
+        index: index,
+        att_srv: att_data,
+    };
+    trigger::mount(top_class, trigger_wrap).await?;
 
     log_debug_mount_end!(logger);
     Ok(())
