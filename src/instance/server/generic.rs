643a3bcdc9cfa7270ebb0cef6a34468af7cb0728
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2025-06-17 07:18:58 +0200

    upp

--- src/instance/server/generic.rs
+++ src/instance/server/generic.rs
@@ -1,4 +1,5 @@
 use super::{CallbackEntry, CallbackId};
+use crate::AlertNotification;
 use crate::Error;
 use crate::Logger;
 use crate::Notification;
@@ -25,6 +26,9 @@ pub struct GenericAttributeServer<B: GenericBuffer> {
     /// Next callback ID
     next_callback_id: Arc<Mutex<CallbackId>>,
 
+    /// Topic of the attribute
+    topic: String,
+
     /// Command topic
     cmd_topic: String,
 
@@ -32,7 +36,7 @@ pub struct GenericAttributeServer<B: GenericBuffer> {
     notification_channel: Sender<Notification>,
 
     /// Current value
-    current_value: Arc<Mutex<Option<B>>>,
+    current_value: Arc<Mutex<B>>,
 }
 
 impl<B: GenericBuffer> GenericAttributeServer<B> {
@@ -55,7 +59,7 @@ impl<B: GenericBuffer> GenericAttributeServer<B> {
 
         //
         //
-        let query_value = Arc::new(Mutex::new(None));
+        let query_value = Arc::new(Mutex::new(B::default()));
 
         // create a queryable to get value at initialization
         //
@@ -63,18 +67,13 @@ impl<B: GenericBuffer> GenericAttributeServer<B> {
         let session_clone = session.clone();
         let query_value_clone = query_value.clone();
         let handle = tokio::spawn(async move {
-            let queryable = session_clone
-                .declare_queryable(format!("{}/att", topic_clone.clone()))
-                .await
-                .unwrap();
+            let att_topic = format!("{}/att", topic_clone.clone());
+
+            let queryable = session_clone.declare_queryable(&att_topic).await.unwrap();
 
             while let Ok(query) = queryable.recv_async().await {
-                // let value = query_value_clone.lock().unwrap().clone(); // Clone the value
-                // let pyl = Bytes::from(serde_json::to_string(&value).unwrap());
-                // query
-                //     .reply(format!("{}/att", topic_clone.clone()), pyl)
-                //     .await
-                //     .unwrap();
+                let p = query_value_clone.lock().await.to_zbytes();
+                query.reply(&att_topic, p).await.unwrap();
             }
             Ok(())
         });
@@ -129,84 +128,12 @@ impl<B: GenericBuffer> GenericAttributeServer<B> {
             session: session,
             callbacks: callbacks,
             next_callback_id: Arc::new(Mutex::new(0)),
+            topic: topic,
             cmd_topic: cmd_topic,
             notification_channel: notification_channel,
             current_value: query_value.clone(),
         }
     }
-
-    // /// Create a new instance
-    // pub async fn new(session: Session) -> Self {
-    //     // Initialize async callbacks storage
-    //     let callbacks = Arc::new(Mutex::new(HashMap::<CallbackId, CallbackEntry<B>>::new()));
-
-    //     // Trigger the callback mechanism on message reception
-    //     let att_topic = format!("{}/att", &metadata.topic);
-    //     let subscriber = session.declare_subscriber(&att_topic).await.unwrap();
-    //     let last_value = Arc::new(Mutex::new(None));
-
-    //     tokio::spawn({
-    //         let callbacks = callbacks.clone();
-    //         let last_value = last_value.clone();
-    //         async move {
-    //             while let Ok(sample) = subscriber.recv_async().await {
-    //                 // Create Buffer from the received zbytes
-    //                 let buffer = B::from_zbytes(sample.payload().clone());
-
-    //                 // Update the last received value
-    //                 {
-    //                     let mut last = last_value.lock().await;
-    //                     *last = Some(buffer.clone());
-    //                 }
-
-    //                 // Trigger all async callbacks
-    //                 let callbacks_map = callbacks.lock().await;
-    //                 let mut futures = Vec::new();
-
-    //                 for (_id, callback_entry) in callbacks_map.iter() {
-    //                     // Check condition if present
-    //                     let should_trigger = if let Some(condition) = &callback_entry.condition {
-    //                         condition(&buffer)
-    //                     } else {
-    //                         true
-    //                     };
-
-    //                     if should_trigger {
-    //                         futures.push((callback_entry.callback)(buffer.clone()));
-    //                     }
-    //                 }
-
-    //                 // Drop the lock before awaiting futures
-    //                 drop(callbacks_map);
-
-    //                 // Execute all callbacks concurrently
-    //                 futures::future::join_all(futures).await;
-    //             }
-    //         }
-    //     });
-
-    //     // Wait for the first message if mode is not WriteOnly
-    //     if metadata.mode != AttributeMode::WriteOnly {
-    //         let query = session.get(&att_topic).await.unwrap();
-    //         let result = query.recv_async().await.unwrap();
-    //         let buffer = B::from_zbytes(result.result().unwrap().payload().clone());
-    //         let mut last = last_value.lock().await;
-    //         *last = Some(buffer);
-    //     }
-
-    //     // Create the command topic
-    //     let cmd_topic = format!("{}/cmd", &metadata.topic);
-
-    //     // Return attribute
-    //     Self {
-    //         session,
-    //         // metadata,
-    //         callbacks,
-    //         next_callback_id: Arc::new(Mutex::new(0)),
-    //         cmd_topic,
-    //         last_value,
-    //     }
-    // }
 
     ///
     ///
@@ -223,95 +150,18 @@ impl<B: GenericBuffer> GenericAttributeServer<B> {
             .unwrap();
 
         // update the current queriable value
-        *self.current_value.lock().await = Some(buffer);
+        *self.current_value.lock().await = buffer;
 
         Ok(())
     }
 
-    // /// Send command and do not wait for validation
-    // pub async fn shoot<T>(&mut self, value: T)
-    // where
-    //     T: Into<B>,
-    // {
-    //     let buffer: B = value.into();
-    //     let publisher = self
-    //         .session
-    //         .declare_publisher(&self.cmd_topic)
-    //         .await
-    //         .unwrap();
-
-    //     publisher.put(buffer.to_zbytes()).await.unwrap();
-    // }
-
-    // /// Send command and wait for validation
-    // pub async fn set<T>(&mut self, value: T) -> Result<(), String>
-    // where
-    //     T: Into<B> + Clone,
-    //     B: PartialEq,
-    // {
-    //     let buffer: B = value.into();
-    //     let expected_buffer = buffer.clone();
-
-    //     self.shoot(buffer).await;
-
-    //     // Wait for the value to be confirmed
-    //     self.wait_for_value(
-    //         move |received_buffer| *received_buffer == expected_buffer,
-    //         Some(std::time::Duration::from_secs(5)),
-    //     )
-    //     .await?;
-
-    //     Ok(())
-    // }
-
-    // /// Get last received value
-    // pub async fn get(&self) -> Option<B> {
-    //     let last = self.last_value.lock().await;
-    //     last.clone()
-    // }
-
-    // /// Wait for a specific value with optional timeout
-    // pub async fn wait_for_value<F>(
-    //     &self,
-    //     condition: F,
-    //     timeout: Option<std::time::Duration>,
-    // ) -> Result<B, String>
-    // where
-    //     F: Fn(&B) -> bool + Send + Sync + 'static,
-    // {
-    //     // Use a broadcast channel to avoid the move issue
-    //     let (tx, mut rx) = tokio::sync::broadcast::channel(1);
-
-    //     // Add temporary callback
-    //     let callback_id = self
-    //         .add_callback(
-    //             move |buffer: B| {
-    //                 let buffer_clone = buffer.clone();
-    //                 let tx_clone = tx.clone();
-    //                 Box::pin(async move {
-    //                     let _ = tx_clone.send(buffer_clone);
-    //                 })
-    //             },
-    //             Some(condition),
-    //         )
-    //         .await;
-
-    //     let result = if let Some(duration) = timeout {
-    //         tokio::time::timeout(duration, rx.recv()).await
-    //     } else {
-    //         // No timeout: wait indefinitely
-    //         Ok(rx.recv().await)
-    //     };
-
-    //     // Remove the callback
-    //     self.remove_callback(callback_id).await;
-
-    //     match result {
-    //         Ok(Ok(buffer)) => Ok(buffer),
-    //         Ok(Err(_)) => Err("Channel closed unexpectedly".to_string()),
-    //         Err(_) => Err("Timeout waiting for value".to_string()),
-    //     }
-    // }
+    ///
+    ///
+    pub async fn trigger_alert<T: Into<String>>(&self, message: T) {
+        let notification =
+            Notification::Alert(AlertNotification::new(self.topic.clone(), message.into()));
+        self.notification_channel.send(notification).await.unwrap();
+    }
 
     /// Add an async callback that will be triggered when receiving buffer messages
     /// Optionally, a condition can be provided to filter when the callback is triggered
